8920173583fd3edf1a85995d65803792787a5b8e
ErzaQorri <93944366+ErzaQorrii@users.noreply.github.com>
2025-01-21 14:08:38 +0100

    Update Edit Button to navigate to ActivityForm via route and Create Button to go to Activities via route

--- API/client-app/src/features/activities/dashboard/details/ActivityDetails.tsx
+++ API/client-app/src/features/activities/dashboard/details/ActivityDetails.tsx
@@ -12,7 +12,7 @@ import {
 import { useStore } from "../../../../app/stores/store";
 import LoadingComponent from "../../../../layout/LoadingComponent";
 import { observer } from "mobx-react-lite";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 export default observer(function ActivityDetails() {
   const { activityStore } = useStore();
@@ -41,8 +41,8 @@ export default observer(function ActivityDetails() {
       </CardContent>
       <CardContent extra>
         <Button.Group widths="2">
-          <Button basic color="blue" content="Edit" />
-          <Button basic color="grey" content="Cancle" />
+          <Button as={Link} to={`/manage/${activity.id}`}  basic color="blue" content="Edit" />
+          <Button as={Link} to={`/activities/`} basic color="grey" content="Cancle" />
         </Button.Group>
       </CardContent>
     </Card>

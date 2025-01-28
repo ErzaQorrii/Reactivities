4b81256173bb42635edb28edd0ac8e7d72076c23
ErzaQorri <93944366+ErzaQorrii@users.noreply.github.com>
2025-01-28 14:56:30 +0100

    feat: Separate Home Page from Navigation and Add Basic Navigation Link

--- API/client-app/src/layout/App.tsx
+++ API/client-app/src/layout/App.tsx
@@ -1,23 +1,27 @@
-
-import { Fragment, useEffect, useState } from 'react';
-import { Container } from 'semantic-ui-react';
-import NavBar from './NavBar';
-import { observer } from 'mobx-react-lite';
-import { Outlet } from 'react-router-dom';
+import { Fragment, useEffect, useState } from "react";
+import { Container } from "semantic-ui-react";
+import NavBar from "./NavBar";
+import { observer } from "mobx-react-lite";
+import { Outlet, useLocation } from "react-router-dom";
+import HomePage from "../features/home/HomePage";
 
 function App() {
+  const location = useLocation();
 
   return (
-    <Fragment>
-      <Container style={{ marginTop: '7em' }}>
-
-
-        <NavBar />
-      <Outlet/>
-      </Container>
-    </Fragment>
-
-  )
+    <>
+      {location.pathname === "/" ? (
+        <HomePage />
+      ) : (
+        <>
+          <Container style={{ marginTop: "7em" }}>
+            <NavBar />
+            <Outlet />
+          </Container>
+        </>
+      )}
+    </>
+  );
 }
 
-export default observer(App)
+export default observer(App);

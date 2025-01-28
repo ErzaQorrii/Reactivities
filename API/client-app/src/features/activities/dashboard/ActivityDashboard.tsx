5a48b6bba36e09e3bc6bfae8ab47fd767fcd5fd4
ErzaQorri <93944366+ErzaQorrii@users.noreply.github.com>
2025-01-28 14:53:55 +0100

    Update useEffect to prevent redundant API calls for activity loading

--- API/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ API/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -8,10 +8,14 @@ import LoadingComponent from "../../../layout/LoadingComponent";
 export default observer(function ActivityDashboard() {
   const { activityStore } = useStore();
   // const { selectedActivity, editMode } = activityStore;
+  const { loadActivities, activityRegistry } = activityStore;
 
   useEffect(() => {
-    activityStore.loadActivities();
-  }, [activityStore]);
+    if(activityRegistry.size <=1)
+    {
+      loadActivities();
+    }
+  }, [loadActivities,activityRegistry]);
 
   if (activityStore.loadingInitial)
     return <LoadingComponent content="Loading app" />;
